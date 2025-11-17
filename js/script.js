eb2e9552005064c5f5f39ed6447900176bd682d6
kaycee5k <kayceeonyia5k@gmail.com>
2025-11-17 10:15:58 +0100

    updates to hamburger for mobile responsive

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const navbar = document.querySelector(".navbar");
+const hamburger = document.querySelector(".hamburger");
+
+navbar.addEventListener("click", () => {
+  navbar.classList.toggle("active");
+});
