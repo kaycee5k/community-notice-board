c824f62efbc656e8caf1534c0b7400ff4b423523
Ebubedike <chidiebube404@gmail.com>
2025-12-03 17:05:13 +0100

    added the javascript functions that will enable the flow of thetestimonial slide.

--- js/script.js
+++ js/script.js
@@ -5,3 +5,18 @@ navbar.addEventListener("click", () => {
   navbar.classList.toggle("active");
 });
 
+// testimonial section
+const testimonials = document.querySelectorAll(".testimonial");
+    let index = 0;
+
+    document.getElementById("nextBtn").addEventListener("click", () => {
+      testimonials[index].classList.remove("active");
+      index = (index + 1) % testimonials.length;
+      testimonials[index].classList.add("active");
+    });
+
+    document.getElementById("prevBtn").addEventListener("click", () => {
+      testimonials[index].classList.remove("active");
+      index = (index - 1 + testimonials.length) % testimonials.length;
+      testimonials[index].classList.add("active");
+    });
