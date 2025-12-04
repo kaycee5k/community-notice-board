a0a96108b4f4b25db8a2d1164b62888e5edf8e6a
Ifeanyi Amah <amahifeanyi@gmail.com>
2025-12-04 14:07:48 +0100

    add a icon for no post yet, add a class to make the card view centralize

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -244,7 +244,8 @@ function displayPosts() {
 // Render posts to DOM
 function renderPosts(postsToRender) {
   if (postsToRender.length === 0) {
-    postContainer.innerHTML = "<p style='text-align:center; font-weight: bold;'>No posts yet.</p>";
+    postContainer.innerHTML = "<div class='post'><img class='image' src='../assets/images/post.png' style='' /><p style=''>No posts yet.</p></div>";
+    // postContainer.innerHTML = postContainer.innerHTML = '<p class="post" style="background-image: url(\'../assets/images/post.png\');">No posts yet.</p>';
     return;
   }
 
